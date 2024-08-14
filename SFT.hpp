5036f9193799a7ccdcba313722237ab5597122ff
dementive <87823030+dementive@users.noreply.github.com>
2024-08-14 14:28:26 -0400

    Include file and line of each failed test in output

--- SFT.hpp
+++ SFT.hpp
@@ -11,36 +11,38 @@
 
 #ifdef TESTS_ENABLED
 
-#define TEST_FAIL_MESSAGE "%-55s | [color=red]Failed:[/color] [b]%s[/b]"
-#define TEST_PASS_MESSAGE "%-55s | [color=green]Passed[/color] %s"
-#define TEST_CASE_NAME_MESSAGE "[color=blue]%-20s[/color]"
-#define ONE_HUNDRED_FIFTY_DASHES                                                                                                                                    \
-    "------------------------------------------------------------------------------------------------------------------------------------------------------"
-
-inline int SFT_test_case_number = 1;
-
 #define VAR_CHECK(first, second) first == godot::Variant(second)
 #define STRING_CHECK(first, second) first == godot::StringName(second)
 #define NULL_CHECK(first) first != nullptr
 #define NUM_CHECK(first, second) first == second
 
-#define NAMED_TESTS(test_case_name, ...)                                                                                                                            \
-    godot::UtilityFunctions::print(ONE_HUNDRED_FIFTY_DASHES);                                                                                                       \
-    godot::UtilityFunctions::print_rich(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                    \
+#define TEST_FAIL_MESSAGE "%-55s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - [code]%s[/code]"
+#define TEST_PASS_MESSAGE "%-55s | [color=green]Passed[/color] %s%s%s"
+#define TEST_CASE_NAME_MESSAGE "[color=blue]%-20s[/color]"
+#define TEST_MESSAGE(condition) condition ? TEST_PASS_MESSAGE : TEST_FAIL_MESSAGE
+#define TEST_FILE(condition) condition ? "" : __FILE__
+#define TEST_LINE(condition) condition ? "" : godot::vformat("%s", __LINE__)
+#define TEST_CONDITION(condition) condition ? "" : #condition
+#define TWO_HUNDRED_DASHES "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+inline int SFT_check_number = 1;
+
+#define NAMED_TESTS(test_case_name, ...)                                                                                                                                      \
+    godot::UtilityFunctions::print(TWO_HUNDRED_DASHES);                                                                                                                       \
+    godot::UtilityFunctions::print_rich(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                              \
     FOR_EACH_THREE(CHECK_NAMED, test_case_name, __VA_OPT__(__VA_ARGS__, ))
 
-#define TESTS(test_case_name, ...)                                                                                                                                  \
-    SFT_test_case_number = 1;                                                                                                                                       \
-    godot::UtilityFunctions::print(ONE_HUNDRED_FIFTY_DASHES);                                                                                                       \
-    godot::UtilityFunctions::print_rich(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                    \
+#define TESTS(test_case_name, ...)                                                                                                                                            \
+    SFT_check_number = 1;                                                                                                                                                     \
+    godot::UtilityFunctions::print(TWO_HUNDRED_DASHES);                                                                                                                       \
+    godot::UtilityFunctions::print_rich(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                              \
     FOR_EACH_TWO(CHECK, test_case_name, __VA_OPT__(__VA_ARGS__, ))
 
-#define CHECK(test_case_name, condition)                                                                                                                            \
-    godot::UtilityFunctions::print_rich(godot::vformat(condition ? TEST_PASS_MESSAGE : TEST_FAIL_MESSAGE, #condition, condition ? "" : #condition));                \
-    SFT_test_case_number++;
+#define CHECK(test_case_name, condition)                                                                                                                                      \
+    godot::UtilityFunctions::print_rich(godot::vformat(TEST_MESSAGE(condition), SFT_check_number, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition)));    \
+    SFT_check_number++;
 
-#define CHECK_NAMED(test_case_name, check_name, condition)                                                                                                          \
-    godot::UtilityFunctions::print_rich(godot::vformat(condition ? TEST_PASS_MESSAGE : TEST_FAIL_MESSAGE, check_name, condition ? "" : #condition));
+#define CHECK_NAMED(test_case_name, check_name, condition)                                                                                                                    \
+    godot::UtilityFunctions::print_rich(godot::vformat(TEST_MESSAGE(condition), check_name, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition)));
 
 #endif // TESTS_ENABLED
 
