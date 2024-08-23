1b2c554099d7934687c9260f5942acd5087338e7
dementive <87823030+dementive@users.noreply.github.com>
2024-08-23 10:34:44 -0400

    Bugfixes

--- SFT.hpp
+++ SFT.hpp
@@ -19,17 +19,17 @@
 #define PRINT_TEST(message, name, file, line, condition) TEST_PRINT_FUNCTION(godot::vformat(message, name, file, line, condition));
 #define TEST_FAIL_MESSAGE "%-55s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - [code]%s[/code]"
 #define TEST_PASS_MESSAGE "%-55s | [color=green]Passed[/color] %s%s%s"
-#define TEST_CASE_NAME_MESSAGE "[color=blue]%s[/color]"
+#define TEST_CASE_NAME_MESSAGE "[color=cyan]%s[/color]"
 
 #define SCENE_TEST_FAIL_MESSAGE(scene_path, message)                                                                                                                          \
-    TEST_PRINT_FUNCTION(godot::vformat("\n[color=blue]Scene Test[/color]: %-43s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - %s %s.",                          \
-            scene_path.replace("res://", ""), __FILE__, __LINE__, scene_path, message))
+	TEST_PRINT_FUNCTION(godot::vformat("\n[color=cyan]Scene Test[/color]: %-43s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - %s %s.",                          \
+			scene_path.replace("res://", ""), __FILE__, __LINE__, scene_path, message))
 #define SCENE_TEST_PASS_MESSAGE(scene_path)                                                                                                                                   \
-    TEST_PRINT_FUNCTION(godot::vformat("\n[color=blue]Scene Test[/color]: %-43s | [color=green]Passed[/color]", scene_path.replace("res://", "")))
+	TEST_PRINT_FUNCTION(godot::vformat("\n[color=cyan]Scene Test[/color]: %-43s | [color=green]Passed[/color]", scene_path.replace("res://", "")))
 
 #define TEST_PASS(test_name) TEST_PRINT_FUNCTION(godot::vformat("%-55s | [color=green]Passed[/color]", test_name));
 #define TEST_FAIL(test_name, message)                                                                                                                                         \
-    TEST_PRINT_FUNCTION(godot::vformat("%-55s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - %s", test_name, __FILE__, __LINE__, message));
+	TEST_PRINT_FUNCTION(godot::vformat("%-55s | [color=red]Failed[/color] [[color=yellow]%s:%s[/color]] - %s", test_name, __FILE__, __LINE__, message));
 
 // Defines to use in your test conditions and checks
 #define VAR_CHECK(first, second) first == godot::Variant(second)
@@ -51,10 +51,10 @@ This effectively makes it so:
 So I think this is a phenomenal use case for goto. Clangd will also let you know if the label doesn't exist so it's pretty hard to mess up and easy to debug.
 */
 #define NULL_CHECK(object)                                                                                                                                                    \
-    if (object == nullptr) {                                                                                                                                                  \
-        TEST_FAIL(godot::vformat("%s nullptr check", #object), godot::vformat("%s is a nullptr!", #object))                                                                   \
-        goto null_##object;                                                                                                                                                   \
-    }
+	if (object == nullptr) {                                                                                                                                                  \
+		TEST_FAIL(godot::vformat("%s nullptr check", #object), godot::vformat("%s is a nullptr!", #object))                                                                   \
+		goto null_##object;                                                                                                                                                   \
+	}
 
 /*
 Make sure to use TEST_OBJECT_END on object_name after this at some point or it won't compile.
@@ -62,33 +62,33 @@ Note that because of the goto usage in NULL_CHECK if you declare any variables a
 "{}"" or the goto will not compile. More info here: https://stackoverflow.com/a/14274292
 Example:
 _ALWAYS_INLINE_ void test_gui() {
-    TEST_OBJECT(Control, main_menu)
-    {
-        int x = 0; // Declaring this variable makes it so you have to do a scoped block :(
-        main_menu->get_name();
-        NAMED_TESTS(
-            "main_menu_tests",
-            "MainMenu get_name", STRING_CHECK(main_menu->get_name(), "Menu"),
-        )
-    }
-    TEST_OBJECT_END(main_menu)
+	TEST_OBJECT(Control, main_menu)
+	{
+		int x = 0; // Declaring this variable makes it so you have to do a scoped block :(
+		main_menu->get_name();
+		NAMED_TESTS(
+			"main_menu_tests",
+			"MainMenu get_name", STRING_CHECK(main_menu->get_name(), "Menu"),
+		)
+	}
+	TEST_OBJECT_END(main_menu)
 
-    // If you do the same thing without any variable declarations you don't need the scoped block.
-    TEST_OBJECT(Control, loading_screen)
-    loading_screen->get_name();
-    NAMED_TESTS(
-        "loading_screen_tests",
-        "LoadingScreen get_name", STRING_CHECK(star->loading_screen(), "LoadingScreen"),
-    )
-    TEST_OBJECT_END(loading_screen)
+	// If you do the same thing without any variable declarations you don't need the scoped block.
+	TEST_OBJECT(Control, loading_screen)
+	loading_screen->get_name();
+	NAMED_TESTS(
+		"loading_screen_tests",
+		"LoadingScreen get_name", STRING_CHECK(star->loading_screen(), "LoadingScreen"),
+	)
+	TEST_OBJECT_END(loading_screen)
 }
 */
 #define TEST_OBJECT(class_name, object_name)                                                                                                                                  \
-    class_name *object_name = memnew(class_name());                                                                                                                           \
-    NULL_CHECK(object_name)
+	class_name *object_name = memnew(class_name());                                                                                                                           \
+	NULL_CHECK(object_name)
 
 #define TEST_OBJECT_END(object_name) null_##object_name : memdelete(object_name);
-#define TEST_SCENE_END(object_name) null_##object_name :;
+#define TEST_SCENE_END(object_name) null_##object_name : object_name->queue_free();
 
 // Defines you or I will never need to change (hopefully)
 #define TEST_MESSAGE(condition) condition ? TEST_PASS_MESSAGE : TEST_FAIL_MESSAGE
@@ -101,43 +101,43 @@ inline int SFT_check_number = 1;
 // The nullptr check does return though to prevent object_name access crashing the program.
 // TEST_SCENE works pretty much exactly like TEST_OBJECT. Make sure to call TEST_SCENE_END at the end of the test.
 #define TEST_SCENE(scene_path, root_node_class_name, object_name)                                                                                                             \
-    root_node_class_name *object_name;                                                                                                                                        \
-    do {                                                                                                                                                                      \
-        if (!ResourceLoader::get_singleton()->exists(scene_path)) {                                                                                                           \
-            SCENE_TEST_FAIL_MESSAGE(String(scene_path), "does not exist");                                                                                                    \
-            break;                                                                                                                                                            \
-        }                                                                                                                                                                     \
-        Ref<PackedScene> scene_ref##_object_name = ResourceLoader::get_singleton()->load(scene_path);                                                                         \
-        if (!scene_ref##_object_name->can_instantiate()) {                                                                                                                    \
-            SCENE_TEST_FAIL_MESSAGE(String(scene_path), "could not be instantiated");                                                                                         \
-            break;                                                                                                                                                            \
-        }                                                                                                                                                                     \
-        Node *node##_object_name = scene_ref##_object_name->instantiate();                                                                                                    \
-        object_name = Object::cast_to<root_node_class_name>(node##_object_name);                                                                                              \
-        if (object_name == nullptr) {                                                                                                                                         \
-            SCENE_TEST_FAIL_MESSAGE(String(#root_node_class_name), "node is a nullptr");                                                                                      \
-            goto null_##object_name;                                                                                                                                          \
-        }                                                                                                                                                                     \
-        SCENE_TEST_PASS_MESSAGE(String(scene_path));                                                                                                                          \
-    } while (0);
+	root_node_class_name *object_name;                                                                                                                                        \
+	do {                                                                                                                                                                      \
+		if (!ResourceLoader::get_singleton()->exists(scene_path)) {                                                                                                           \
+			SCENE_TEST_FAIL_MESSAGE(String(scene_path), "does not exist");                                                                                                    \
+			break;                                                                                                                                                            \
+		}                                                                                                                                                                     \
+		Ref<PackedScene> scene_ref##_object_name = ResourceLoader::get_singleton()->load(scene_path, "PackedScene", ResourceLoader::CACHE_MODE_IGNORE_DEEP);                  \
+		if (!scene_ref##_object_name->can_instantiate()) {                                                                                                                    \
+			SCENE_TEST_FAIL_MESSAGE(String(scene_path), "could not be instantiated");                                                                                         \
+			break;                                                                                                                                                            \
+		}                                                                                                                                                                     \
+		Node *node##_object_name = scene_ref##_object_name->instantiate();                                                                                                    \
+		object_name = Object::cast_to<root_node_class_name>(node##_object_name);                                                                                              \
+		if (object_name == nullptr) {                                                                                                                                         \
+			SCENE_TEST_FAIL_MESSAGE(String(#root_node_class_name), "node is a nullptr");                                                                                      \
+			goto null_##object_name;                                                                                                                                          \
+		}                                                                                                                                                                     \
+		SCENE_TEST_PASS_MESSAGE(String(scene_path));                                                                                                                          \
+	} while (0);
 
 // The actual tests
 #define NAMED_TESTS(test_case_name, ...)                                                                                                                                      \
-    TEST_PRINT_FUNCTION(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                                              \
-    FOR_EACH_THREE(CHECK_NAMED, test_case_name, __VA_OPT__(__VA_ARGS__, ))
+	TEST_PRINT_FUNCTION(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                                              \
+	FOR_EACH_THREE(CHECK_NAMED, test_case_name, __VA_OPT__(__VA_ARGS__, ))
 
 #define TESTS(test_case_name, ...)                                                                                                                                            \
-    SFT_check_number = 1;                                                                                                                                                     \
-    TEST_PRINT_FUNCTION(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                                              \
-    FOR_EACH_TWO(CHECK, test_case_name, __VA_OPT__(__VA_ARGS__, ))
+	SFT_check_number = 1;                                                                                                                                                     \
+	TEST_PRINT_FUNCTION(godot::vformat(TEST_CASE_NAME_MESSAGE, test_case_name));                                                                                              \
+	FOR_EACH_TWO(CHECK, test_case_name, __VA_OPT__(__VA_ARGS__, ))
 
 // The condition checks that the tests perform
 #define CHECK(test_case_name, condition)                                                                                                                                      \
-    PRINT_TEST(TEST_MESSAGE(condition), SFT_check_number, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition))                                              \
-    SFT_check_number++;
+	PRINT_TEST(TEST_MESSAGE(condition), SFT_check_number, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition))                                              \
+	SFT_check_number++;
 
 #define CHECK_NAMED(test_case_name, check_name, condition)                                                                                                                    \
-    PRINT_TEST(TEST_MESSAGE(condition), check_name, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition))
+	PRINT_TEST(TEST_MESSAGE(condition), check_name, TEST_FILE(condition), TEST_LINE(condition), TEST_CONDITION(condition))
 
 #endif // TESTS_ENABLED
 
